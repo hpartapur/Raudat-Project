5892d06e4dfc1372819588a57e006247b47e781d
Hatim P <hpartapur@gmail.com>
2022-04-24 18:09:37 +0300

    First Release

--- sketch2.js
+++ sketch2.js
@@ -11,6 +11,9 @@ var seconds;
 var original_seconds;
 var counting;
 var clicktime;
+var elapsed;
+
+
 
 
 function setup(){
@@ -25,8 +28,10 @@ function setup(){
     original_seconds=30;  //Total seconds in countdown timer, mutable.
     seconds=original_seconds //How many seconds left in the timer.
     counting=false;  //Has countdown started?
+    clicktime=0
+    elapsed=0;
 
-    kawakib=loadImage("kawakibcreations.png") //Logo
+
 }
 function draw(){
     background(245,222,179);
@@ -38,24 +43,24 @@ function draw(){
     text(arabic[1],width*1/8, height*1.5/8)//Yasaar
 
 
-    image(kawakib,width-100,height-100,90,90)//Logo
+
 
     textSize(300)
     fill("blue")
     text(score_right, width*5.5/8, height*3.5/5); //Yameen Score
     text(score_left, width*1/8, height*3.5/5);  //Yasaar Score
+    
+    
+    textSize(15)
+    text("Kawakib Creations", width*6.5/8, height*7.5/8)
 
 
 
 
 
-    mheight=height-(((height-100)/original_seconds)*(original_seconds-seconds))
 
 
-    stroke(0)
-    strokeWeight(21);
-    line(width/2, 0, width/2, mheight);
-    strokeWeight(1);
+
 
 
 
@@ -63,41 +68,37 @@ function draw(){
     textSize(50)
     fill(128,0,128)
     text(misra, width/2, height*7/8)
+    
+    if (counting==true){elapsed=(Math.round(millis()/1000))-clicktime}
+    
+    
+    if (counting==true&&elapsed==original_seconds){
+        console.log("TIME UP!")
+        counting=false;
+    }
+    
+    text(original_seconds-elapsed, width* 3/7, 200)
+//    arc(50, 50, 80, 80, 0, 1.6);
+//    
 
-
-
-    fill(0)
-    textSize(100)
-    ellipse(width/2,
-            mheight-40
-            ,120,120)
-    fill(255,
-        255-8.5*(30-seconds),
-        255-8.5*(30-seconds))
-    text(seconds, width/2-50,mheight)
 
 
 
 
 
 }
-function decsec (){
-    if (seconds>0 && counting==true){seconds--}
-    if (seconds==0){reset()}
 
-}
-function t(){
-    counting=true;
-    setInterval(decsec, 1000)
-}
 
 function mousePressed(){
 
 
-    if(dist(mouseX, mouseY, width/2, mheight)<100){
-        t()//Activate Countdown
-        startCounting()
-    }
+    if(dist(mouseX, mouseY, width/2, height/2)<1000&& counting==false){
+			clicktime=Math.round(millis()/1000)
+			console.log(clicktime)
+            counting=true;
+		}
+
+
     if(dist(mouseX, mouseY, 753,80)<70){
         score_right++
     }
@@ -115,13 +116,4 @@ function bait(team,misra1){
     if (team==2){
         score_left++
     }
-}
-function reset(){
-    seconds=original_seconds;
-    counting=false;
-}
-
-function startCounting (){
-    clicktime=millis()
-    timeup=clicktime+(original_seconds*1000)
 }
